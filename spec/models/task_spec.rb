b046fc59c84fe8e8c0e31330117c773c137d4272
Rattuti <i.ishiharar.96296@gmail.com>
2022-08-07 15:30:16 +0900

    init

---
+++ spec/models/task_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe 'タスクモデル機能', type: :model do
+  describe 'バリデーションのテスト' do
+    context 'タスクのタイトルが空文字の場合' do
+      it 'バリデーションに失敗する' do
+        task = Task.create(tittle: 'test', content: '')
+        expect(task).not_to be_valid
+      end
+    end
+
+    context 'タスクの説明が空文字の場合' do
+      it 'バリデーションに失敗する' do
+        task = Task.create(tittle: '', content: 'test')
+        expect(task).not_to be_valid
+      end
+    end
+
+    context 'タスクのタイトルと説明に値が入っている場合' do
+      it 'タスクを登録できる' do
+        task = Task.create(tittle: 'test', content: 'test')
+        expect(task).to be_valid
+      end
+    end
+  end
+end
