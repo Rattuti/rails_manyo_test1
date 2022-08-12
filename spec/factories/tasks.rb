a98e9c5927f14a96c5b1c139beb83250c59a34c9
Rattuti <i.ishiharar.96296@gmail.com>
2022-08-13 07:39:14 +0900

    init

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -2,14 +2,19 @@
 FactoryBot.define do
     # 作成するテストデータの名前を「task」とします
     # 「task」のように実際に存在するクラス名と一致するテストデータの名前をつければ、そのクラスのテストデータを作成されます
-    factory :task do
-      tittle { '書類作成' }
-      content { '企画書を作成する。' }
+    factory :first_task, class: Task do
+        tittle { 'test1' }
+        content { 'test1' }
     end
     # 作成するテストデータの名前を「second_task」とします
     # 「second_task」のように存在しないクラス名をつける場合、`class`オプションを使ってどのクラスのテストデータを作成するかを明示する必要がります
     factory :second_task, class: Task do
       tittle { 'メール送信' }
       content { '顧客へ営業のメールを送る。' }
+    end
+
+    factory :third_task, class: Task do
+        tittle { 'test3' }
+        content { 'test3' }
     end
   end
--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -5,44 +5,43 @@ RSpec.describe 'タスク管理機能', type: :system do
     context 'タスクを登録した場合' do
       it '登録したタスクが表示される' do
         visit new_task_path
-        fill_in "Tittle", with: "test"
-        fill_in "Content", with: "test"
-        click_button "Create Task"
-
-        visit new_task_path
-        #expect(page).to have_content "success"        
+        fill_in "タイトル", with: "test"
+        fill_in "内容", with: "test"
+        click_button "登録する"
+        expect(page).to have_content "Show"
       end
     end
   end
 
   describe '一覧表示機能' do
+# let!を使ってテストデータを変数として定義することで、複数のテストでテストデータを共有できる
+    let!(:first_task) { FactoryBot.create(:first_task) }
+    let!(:second_task) { FactoryBot.create(:second_task) }
+    let!(:third_task) { FactoryBot.create(:third_task) }
+    before do
+        visit tasks_path
+    end
+
     context '一覧画面に遷移した場合' do
       it '作成済みのタスク一覧が作成日時の降順で表示される' do
-        # テストで使用するためのタスクを登録
-        #Task.create!(tittle: '書類作成', content: '企画書を作成する。')
-        FactoryBot.create(:task)
-        # タスク一覧画面に遷移
-        visit tasks_path
-        # visit（遷移）したpage（この場合、タスク一覧画面）に"書類作成"という文字列が、have_content（含まれていること）をexpect（確認・期待）する
-        expect(page).to have_content '書類作成'
-        # expectの結果が「真」であれば成功、「偽」であれば失敗としてテスト結果が出力される
+        task_list = all('tbody tr')
+        expect(task_list[0]).to have_content 'test3'
+        expect(task_list[1]).to have_content '書類作成'
+        expect(task_list[2]).to have_content 'test1'
       end
     end
         context '新たにタスクを作成した場合' do
             it '新しいタスクが一番上に表示される' do
+              task_list = all('tbody tr')
+              expect(task_list[0]).to have_content 'test3'
           end
       end
   end
 
-  describe '詳細表示機能1' do
+  describe '詳細表示機能' do
      context '任意のタスク詳細画面に遷移した場合' do
        it 'そのタスクの内容が表示される' do
-        visit new_task_path
-        fill_in "Tittle", with: "test"
-        fill_in "Content", with: "test"
-        click_button "Create Task"
-
-        task = Task.last
+       task = Task.last
         visit task_path(task.id)
         expect(page).to have_content "Show Task Page"
         expect(page).to have_content task.tittle
