7fc83936d8623f360c771d4a31720af75cb450b8
Rattuti <i.ishiharar.96296@gmail.com>
2022-08-17 15:57:38 +0900

    first

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -23,4 +23,35 @@ RSpec.describe 'タスクモデル機能', type: :model do
       end
     end
   end
+  describe '検索機能' do
+    # テストデータを複数作成する
+    let!(:first_task) { FactoryBot.create(:task, title: 'first_task_title') }
+    let!(:second_task) { FactoryBot.create(:second_task, title: "second_task_title") }
+    let!(:third_task) { FactoryBot.create(:third_task, title: "third_task_title") }
+    context 'scopeメソッドでタイトルのあいまい検索をした場合' do
+      it "検索ワードを含むタスクが絞り込まれる" do
+        # toとnot_toのマッチャを使って検索されたものとされなかったものの両方を確認する
+        # 検索されたテストデータの数を確認する
+        # タイトルの検索メソッドをseach_titleとしてscopeで定義した場合のコード例
+        # scopeを使って定義した検索メソッドに検索ワードを挿入する
+        # toとnot_toのマッチャを使って検索されたものとされなかったものの両方を確認する
+        # 検索されたテストデータの数を確認する
+        expect(Task.search_title('first')).to include(first_task)
+        expect(Task.search_title('first')).not_to include(second_task)
+        expect(Task.search_title('first')).not_to include(third_task)
+        expect(Task.search_title('first').count).to eq 1
+      end
+    end
+    context 'scopeメソッドでステータス検索をした場合' do
+      it "ステータスに完全一致するタスクが絞り込まれる" do
+        # toとnot_toのマッチャを使って検索されたものとされなかったものの両方を確認する
+        # 検索されたテストデータの数を確認する
+      end
+    end
+    context 'scopeメソッドでタイトルのあいまい検索とステータス検索をした場合' do
+      it "検索ワードをタイトルに含み、かつステータスに完全一致するタスク絞り込まれる" do
+        # toとnot_toのマッチャを使って検索されたものとされなかったものの両方を確認する
+        # 検索されたテストデータの数を確認する
+      end
+    end
 end
