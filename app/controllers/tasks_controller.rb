de7e57d774d625d1ae2532a38222b83de120aad0
Rattuti <i.ishiharar.96296@gmail.com>
2022-09-02 01:21:04 +0900

    init

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -75,7 +75,7 @@ class TasksController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def task_params
-      params.require(:task).permit(:tittle, :content, :deadline_on, :priority, :status)
+      params.require(:task).permit(:tittle, :content, :deadline_on, :priority, :status, { label_ids: [] })
     end
 
     def task_search_params
